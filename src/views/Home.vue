19500e4e69faa050b2a4224061b2312f57c324b5
ernestomelchor <nurturelove88@gmail.com>
2020-04-21 21:17:41 -0500

    play with vue-country-flag component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>
+      <country-flag country="it" size="big" />
+      <country-flag country="hr" size="big" />
+      <country-flag country="fr" size="big" />
+      <country-flag country="rus" size="big" />
+    </div>
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HelloWorld from "@/components/HelloWorld.vue";
+import CountryFlag from "vue-country-flag";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+    CountryFlag,
+  },
+};
 </script>
